09fcea99b15f713a0efe50b1c848930979e3ebc9
Hristiyan Petkov <hristiyanpetkov@Hristiyans-MacBook-Pro.local>
2022-06-01 13:24:03 +0300

    Initial setup

---
+++ Backend/src/main/java/com/fmi/cinema/cinema/movies/service/MoviesService.java
@@ -0,0 +1,25 @@
+package com.fmi.cinema.cinema.movies.service;
+
+import com.fmi.cinema.cinema.movies.model.MovieCoreInfo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class MoviesService
+{
+
+//	private final MoviesRepository moviesRepository;
+//
+//	public MoviesService(final MoviesRepository moviesRepository)
+//	{
+//		this.moviesRepository = moviesRepository;
+//	}
+
+	public List<MovieCoreInfo> getAllMovies()
+	{
+//		return moviesRepository.findAll();
+		return null;
+	}
+}
