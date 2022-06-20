19619fccc7bd9925d66e97609bd2a399200b3b98
vulgar-chair <81829474+vulgar-chair@users.noreply.github.com>
2022-06-20 20:37:20 +0300

    add missing annotation

--- Backend/src/main/java/com/fmi/cinema/cinema/CinemaApplication.java
+++ Backend/src/main/java/com/fmi/cinema/cinema/CinemaApplication.java
@@ -8,6 +8,5 @@ public class CinemaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
-		System.out.println("SMAAAAAAAAAAAAAASHHHHH");
 	}
 }
--- Backend/src/main/java/com/fmi/cinema/cinema/service/TicketService.java
+++ Backend/src/main/java/com/fmi/cinema/cinema/service/TicketService.java
@@ -1,5 +1,8 @@
 package com.fmi.cinema.cinema.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TicketService {
 
 }
