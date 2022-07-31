08c61c6923ff41df78ee4ef808339a047a3660e2
vulgar-chair <81829474+vulgar-chair@users.noreply.github.com>
2022-07-31 12:53:15 +0300

    add movie controller which handles GET POST PUT DELETE requests for movies

--- Backend/src/main/java/com/fmi/cinema/cinema/controller/MovieController.java
+++ Backend/src/main/java/com/fmi/cinema/cinema/controller/MovieController.java
@@ -1,28 +1,57 @@
 package com.fmi.cinema.cinema.controller;
 
-import com.fmi.cinema.cinema.model.Movie;
+import com.fmi.cinema.cinema.model.dto.moviedto.MovieDto;
 import com.fmi.cinema.cinema.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
-public class MovieController
-{
+public class MovieController {
 
-	private final MovieService movieService;
+    private final MovieService movieService;
 
-	public MovieController(final MovieService movieService)
-	{
-		this.movieService = movieService;
-	}
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
 
-	@GetMapping
-	public List<Movie> getAllMovies()
-	{
-		return movieService.getAllMovies();
-	}
+    @GetMapping("/{id}")
+    public MovieDto getMovieById(@PathVariable final Long id) {
+        return movieService.findById(id);
+    }
+
+    @GetMapping
+    public List<MovieDto> getAllMovies() {
+        return movieService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MovieDto addMovie(@Valid @RequestBody final MovieDto requestDto) {
+        return movieService.addMovie(requestDto);
+    }
+
+    @PutMapping
+    public MovieDto updateMovie(@Valid @RequestBody final MovieDto requestDto) {
+        return movieService.updateMovie(requestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMovie(@PathVariable final Long id) {
+        movieService.removeMovie(id);
+    }
 }
