e3ec960137f62cbe0da96a75800cfa9dbf7a5868
vulgar-chair <81829474+vulgar-chair@users.noreply.github.com>
2022-07-31 13:04:52 +0300

    add global exception handler that handles all exceptions

---
+++ Backend/src/main/java/com/fmi/cinema/cinema/advice/GlobalRestExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.fmi.cinema.cinema.advice;
+
+import com.fmi.cinema.cinema.exceptions.MovieAlreadyExistsException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalRestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final String ERROR_MESSAGE = "{ \"error\": \"%s\" }";
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Object> handleElementNotFound(NoSuchElementException e) {
+        String responseBody = String.format(ERROR_MESSAGE, e.getMessage());
+        return new ResponseEntity<>(responseBody, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MovieAlreadyExistsException.class)
+    public ResponseEntity<Object> handleMovieAlreadyExists(MovieAlreadyExistsException e) {
+        String responseBody = String.format(ERROR_MESSAGE, e.getMessage());
+        return new ResponseEntity<>(responseBody, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException e,
+            HttpHeaders headers, HttpStatus status,
+            WebRequest request) {
+
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult()
+                .getAllErrors()
+                .forEach((error) -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String message = error.getDefaultMessage();
+                    errors.put(fieldName, message);
+                });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
