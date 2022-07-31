07e703501394945048e0127229bd535cb6e827ff
vulgar-chair <81829474+vulgar-chair@users.noreply.github.com>
2022-07-31 13:06:47 +0300

    update MovieRepository

--- Backend/src/main/java/com/fmi/cinema/cinema/repository/MovieRepository.java
+++ Backend/src/main/java/com/fmi/cinema/cinema/repository/MovieRepository.java
@@ -4,8 +4,14 @@ import com.fmi.cinema.cinema.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Integer>
-{
+import java.util.List;
+import java.util.Optional;
 
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    Optional<Movie> findById(final Long id);
+
+    Optional<Movie> findByName(String name);
+
+    List<Movie> findAll();
 }
